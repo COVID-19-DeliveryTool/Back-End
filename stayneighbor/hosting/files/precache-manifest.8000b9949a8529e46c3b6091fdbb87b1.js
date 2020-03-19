9b1737a2a18bd7be9fab5a6ad258dc1269163eba
Kyle Wilson <kylewilson52@gmail.com>
2020-03-19 19:59:59 -0400

    first commit stitch export

---
+++ stayneighbor/hosting/files/precache-manifest.8000b9949a8529e46c3b6091fdbb87b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1b8da9feda3164641024288e7dcf60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af9fb55aaf34b835357e",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "c9fddfca2687f4268d48",
+    "url": "/static/css/main.18cd2cdd.chunk.css"
+  },
+  {
+    "revision": "af9fb55aaf34b835357e",
+    "url": "/static/js/2.788d6cdb.chunk.js"
+  },
+  {
+    "revision": "283e247fcd70152d45e39e6e5a17cbed",
+    "url": "/static/js/2.788d6cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fddfca2687f4268d48",
+    "url": "/static/js/main.0854e05b.chunk.js"
+  },
+  {
+    "revision": "c4bed11295c6f8245ff1",
+    "url": "/static/js/runtime-main.73fdaa70.js"
+  }
+]);

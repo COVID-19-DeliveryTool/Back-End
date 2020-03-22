f4a2c6918ee34e6b2fd0984d89954d6b2067d4cc
Andrew Minutella <aminutella@ess.engsoftsolutions.local>
2020-03-22 08:33:56 -0400

    Added update order status by order id

---
+++ functions/updateOrderStatus/source.js
@@ -0,0 +1,32 @@
+exports = function(orderId, orderStatus){
+
+  let db = context.services.get("mongodb-atlas").db("stayneighbor")
+  let collection = db.collection("orders")
+  let query = {"_id": BSON.ObjectId(orderId)};
+  let options = {upsert: false}
+  return collection.find(query).toArray()
+  .then(arr => {
+    console.log(arr.length)
+    if (arr.length === 0){
+     return  ({"status": "400", "message": "Order does not exist"});
+    }
+    
+    let update = {
+
+      "$set": {
+        "status": orderStatus 
+      }
+    };
+
+  return collection.updateOne(query, update, options)
+  .then(result => {
+    const { matchedCount, modifiedCount } = result;
+    if(matchedCount && modifiedCount) {
+      return {"status": '200', 'message':"Successfully updated " + result.modifiedCount + " item"}
+    }
+  }).catch(err => {
+      return {"status": '400', 'message':"Failed to update item:" + err}
+    });
+  })
+
+}

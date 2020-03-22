9d69b3015e52aeffd62300a2d105432e7442c378
Patrick <patrick@willetts.dev>
2020-03-22 09:14:01 -0400

    front end build

--- hosting/files/service-worker.js
+++ hosting/files/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.415398231b321deb37d8ec4e3115ee98.js"
+  "/precache-manifest.0e295d614da5b4ab079bee064c1df827.js"
 );
 
 self.addEventListener('message', (event) => {

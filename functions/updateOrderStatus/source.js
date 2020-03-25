2bd4c38b2790a5f394d583e4e0a913ced9343ebe
Kyle Wilson <kylewilson52@gmail.com>
2020-03-24 20:12:14 -0400

    rajn reformat

--- functions/updateOrderStatus/source.js
+++ functions/updateOrderStatus/source.js
@@ -1,32 +1,32 @@
-exports = function(orderId, orderStatus){
+exports = function (orderId, orderStatus) {
 
   let db = context.services.get("mongodb-atlas").db("stayneighbor")
   let collection = db.collection("orders")
-  let query = {"_id": BSON.ObjectId(orderId)};
-  let options = {upsert: false}
+  let query = { "_id": BSON.ObjectId(orderId) };
+  let options = { upsert: false }
   return collection.find(query).toArray()
-  .then(arr => {
-    console.log(arr.length)
-    if (arr.length === 0){
-     return  ({"status": "400", "message": "Order does not exist"});
-    }
-    
-    let update = {
-
-      "$set": {
-        "status": orderStatus 
+    .then(arr => {
+      console.log(arr.length)
+      if (arr.length === 0) {
+        return ({ "status": "400", "message": "Order does not exist" });
       }
-    };
 
-  return collection.updateOne(query, update, options)
-  .then(result => {
-    const { matchedCount, modifiedCount } = result;
-    if(matchedCount && modifiedCount) {
-      return {"status": '200', 'message':"Successfully updated " + result.modifiedCount + " item"}
-    }
-  }).catch(err => {
-      return {"status": '400', 'message':"Failed to update item:" + err}
-    });
-  })
+      let update = {
+
+        "$set": {
+          "status": orderStatus
+        }
+      };
+
+      return collection.updateOne(query, update, options)
+        .then(result => {
+          const { matchedCount, modifiedCount } = result;
+          if (matchedCount && modifiedCount) {
+            return { "status": '200', 'message': "Successfully updated " + result.modifiedCount + " item" }
+          }
+        }).catch(err => {
+          return { "status": '400', 'message': "Failed to update item:" + err }
+        });
+    })
 
 }

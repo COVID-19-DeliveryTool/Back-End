71bbd5087d7cf88a2ee1f592ae87391d45a10be9
Kyle Wilson <kylewilson52@gmail.com>
2020-03-27 23:21:05 -0400

    updated complete order, still some weird error in ui

--- functions/completeOrder/source.js
+++ functions/completeOrder/source.js
@@ -1,20 +1,45 @@
-exports = async function(orderId){
+/**
+let orderId = "5e76aad15f06170a32d8f8b5";
+exports(orderId);
+*/
 
-  let db = context.services.get(context.values.get("cluster-name")).db(context.values.get("db-name"));
-  let collection = db.collection("orders");
+//TODO: add comments
+exports = async function (orderId) {
 
-  if(!orderId) {
-    return { "status": '400', "message": "orderId is a required input and cannot be null/empty" };
-  }
+    let db = context.services.get(context.values.get("cluster-name")).db(context.values.get("db-name"));
+    let orderCollection = db.collection("orders");
+    let findByOrderId = { "_id": BSON.ObjectId(orderId) };
 
-  let order = await db.findOne({"_id": orderId});
+    if (!orderId) {
+        return { "status": '400', "message": "orderId is a required input and cannot be null/empty" };
+    }
 
-  if(!order || !order._id) {
-    return { "status": "404", "message": "Order not found " };
-  }
-  
-  if(order.status !== 'IN PROGRESS') {
-        return { "s}
-  }
+    let order = await orderCollection.findOne(findByOrderId);
 
+    if (!order || !order._id) {
+        return { "status": "404", "message": "Order not found " };
+    }
+
+    //Order should only be moved to complete if it is currently in progess.
+    //TODO: maybe add driver email verification somehow
+    if (order.status !== "IN PROGRESS") {
+        return { "status": "409", "message": "Cannot complete, order must be in 'IN PROGRESS' status, currently in: " + order.status };
+    }
+
+    
+    order.status = "COMPLETED";
+
+    //TODO: notify dispatcher?
+
+    console.log(JSON.stringify(order));
+    orderCollection.replaceOne(
+        findByOrderId,
+        order,
+        { upsert: false}
+    ).then(result => {
+        console.log(JSON.stringify(result));
+        return { "status": "200", "message": "Successfully updated " + result.modifiedCount + " item" };
+    }).catch(err => {
+        return { "status": '400', 'message': "Failed to update item:" + err };
+    });
 }

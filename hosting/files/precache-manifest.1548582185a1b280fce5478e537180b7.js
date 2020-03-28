2e9e5af6f0b8b33bd3429d80c5e1792c74e3d947
Patrick <patrick@willetts.dev>
2020-03-28 12:16:06 -0400

    FE Build #1

--- hosting/files/precache-manifest.1548582185a1b280fce5478e537180b7.js
+++ hosting/files/precache-manifest.1548582185a1b280fce5478e537180b7.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3eb9e024422e4fbffd6d5b428fd6bc0a",
+    "revision": "3300c5cd2d5d5a7599a9a6579a7bea3a",
     "url": "/index.html"
   },
   {
-    "revision": "520c41c77c8deeb7bfcb",
+    "revision": "2f21b98986431fc9c908",
     "url": "/static/css/2.5dbdccff.chunk.css"
   },
   {
-    "revision": "51a75d96ced1a5bcdc0a",
-    "url": "/static/css/main.0d342744.chunk.css"
+    "revision": "d62aa486a64a241b27da",
+    "url": "/static/css/main.1cc0e634.chunk.css"
   },
   {
-    "revision": "520c41c77c8deeb7bfcb",
-    "url": "/static/js/2.d9915074.chunk.js"
+    "revision": "2f21b98986431fc9c908",
+    "url": "/static/js/2.d58db1f9.chunk.js"
   },
   {
     "revision": "a4ded60c5ff61f8c8219d471c70c0d24",
-    "url": "/static/js/2.d9915074.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.d58db1f9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "51a75d96ced1a5bcdc0a",
-    "url": "/static/js/main.5be7320e.chunk.js"
+    "revision": "d62aa486a64a241b27da",
+    "url": "/static/js/main.ea592225.chunk.js"
   },
   {
     "revision": "c4bed11295c6f8245ff1",
